35801672d291aff57c60e439f4f12dc3d88994f8
payamprivate <payamprivate@gmail.com>
2019-07-02 19:43:07 +0430

    Initial Commit

---
+++ revolut_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  revolut_test
+//
+//  Created by payam ghader kurehpaz on 7/2/19.
+//  Copyright © 2019 payam ghader kurehpaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
