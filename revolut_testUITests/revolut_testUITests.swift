d693f2085f5b09bd08d4e63f9982aa5a7c535343
payamprivate <payamprivate@gmail.com>
2019-07-10 01:32:09 +0430

    last update

--- revolut_testUITests/revolut_testUITests.swift
+++ revolut_testUITests/revolut_testUITests.swift
@@ -13,26 +13,14 @@ class revolut_testUITests: XCTestCase {
     let app = XCUIApplication()
     
     override func setUp() {
-
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         
         app.launchArguments = ["enable-testing"]
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        
         app.launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_Currencies_and_add_new_Currency() {
         
         if ( XCUIApplication().staticTexts["Choose a currency pair to compare their live rates"].exists ){
             let app = XCUIApplication()
